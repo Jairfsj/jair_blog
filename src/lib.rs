1192f9c3958a9ede88d061b6e1d13288402e1ff8
jairfsj@gmail.com <Jairfsj>
2025-02-20 20:17:39 -0500

    star blog

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use std::net::TcpListener;
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, middleware};
+pub fn start_blog(listener: TcpListener) -> Result<Server, std::io::Error> {
+	let srv = HttpServer::new(move || {
+	    App::new()
+		   .wrap(middleware::Logger::default()) // enable logger
+		   .route("/health", web::get().to(HttpResponse::Ok))
+	})
+	.listen(listener)?
+	.run();
+	
+	Ok(srv)
+}
